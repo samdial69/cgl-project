13b9edc04e1c784ceacac270ef2934930ffdd74b
samdial69 <sdiallo585gmail.com>
2022-10-05 13:39:24 +0200

    First commit

---
+++ src/main/java/com/example/cglproject/CglprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cglproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CglprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CglprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cglproject/CglprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cglproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CglprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
